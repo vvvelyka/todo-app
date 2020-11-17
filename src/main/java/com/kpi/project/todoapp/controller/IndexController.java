7329b0a7c4d266523ec429c042fd25a92410ddd4
vvvelikaya <nik.velikay@gmail.com>
2020-11-17 23:39:23 +0200

    init

---
+++ src/main/java/com/kpi/project/todoapp/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.kpi.project.todoapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
